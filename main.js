fb343dd7614eb600867839e639afa526b742ff90
DoubleOSafan <147611100+DoubleOSafan@users.noreply.github.com>
2023-12-10 23:59:19 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-// Our football team has finished the championship.
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
 
-// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
-// For example: ["3:1", "2:2", "0:1", ...]
-
-// Points are awarded for each match as follows:
-
-// if x > y: 3 points (win)
-// if x < y: 0 points (loss)
-// if x = y: 1 point (tie)
-// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
-
-// Notes:
-
-// our team always plays 10 matches in the championship
-// 0 <= x <= 4
-// 0 <= y <= 4
-
-function points(games) {
-    let total = 0;
-    games.map(game => {
-      if (game[0] === game[2]) {
-        total += 1;
-      } else if (game[0] > game[2]) {
-        total += 3;
-      }
-    });
-    return total;
+function makeNegative(num) {
+  if(num >= 0) { 
+    return num - num * 2    
   }
+  else {
+    return num
+  }
+}
+//takes in number
+//if number is more or equal to 0: return negative
+//negative: number minus (number times two)
+//else return de number that is already negative
