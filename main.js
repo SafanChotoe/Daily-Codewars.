8138499718766025c2789c6ba536fb5021253e8b
DoubleOSafan <147611100+DoubleOSafan@users.noreply.github.com>
2023-12-11 23:26:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
-
-// Examples
-// makeNegative(1);    // return -1
-// makeNegative(-5);   // return -5
-// makeNegative(0);    // return 0
-// makeNegative(0.12); // return -0.12
-// Notes
-// The number can be negative already, in which case no change is required.
-// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
-
-function makeNegative(num) {
-  if(num >= 0) { 
-    return num - num * 2    
+function evenOrOdd(number) {
+  if(number % 2 === 0) {
+    return "Even"
   }
-  else {
-    return num
+  else{
+    return "Odd"
   }
 }
-//takes in number
-//if number is more or equal to 0: return negative
-//negative: number minus (number times two)
-//else return de number that is already negative
