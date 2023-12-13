b8cd52ad48ee235b6d94ae4edb5633716e87aa3e
Safan Chotoe <147611100+SafanChotoe@users.noreply.github.com>
2023-12-13 17:50:31 +0100

    Add files via upload

--- CodewarsDaily/main.js
+++ CodewarsDaily/main.js
@@ -1,23 +1,8 @@
-// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
-// Examples
-// makeNegative(1);    // return -1
-// makeNegative(-5);   // return -5
-// makeNegative(0);    // return 0
-// makeNegative(0.12); // return -0.12
-// Notes
-// The number can be negative already, in which case no change is required.
-// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
-
-function makeNegative(num) {
-  if(num >= 0) { 
-    return num - num * 2    
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
   }
-  else {
-    return num
-  }
-}
-//takes in number
-//if number is more or equal to 0: return negative
-//negative: number minus (number times two)
-//else return de number that is already negative
+  //takes in name. 
+  //with backticks we use template literal to denote in JS.
