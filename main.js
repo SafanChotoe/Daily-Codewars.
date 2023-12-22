48f43f9c284890a49c7741fb3c6272ddf67a571e
Safan Chotoe <147611100+SafanChotoe@users.noreply.github.com>
2023-12-22 23:56:46 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-// You are given two sorted arrays that both only contain integers. Your task is to find a way to merge them into a single one, sorted in asc order. Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+// The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
 
-// You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
+// Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
 
-// Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+// Example:
 
-// Examples (input -> output)
-// * [1, 2, 3, 4, 5], [6, 7, 8, 9, 10] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// * [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// * [1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12] -> [1, 2, 3, 4, 5, 7, 9, 10, 11, 12]
-function mergeArrays(arr1, arr2) {
-    return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+function duckDuckGoose(players, goose) {
+  
+    return players[(goose-1)%players.length].name
   }
 
-//amazing codewars this one!
-//function takes in two arrays.
-//the concat function merges these two arrays together.
-//the sort method then sorts this merged array.
-//new Set creates the object with unique values, which means that a value may only occur once.
-//then this set is made into an array.
-//this array in the end is sorted, made unique and returned!!
+//the modulo cycles through the array till it reaches the number given.
+//this cycle goes through the indexes of the array, starting from 0 and looping all the way back to 0 if reached the end.
+//index is chosen where loops stops.
+//gets the element from the index in the array.
+//this element is than attached to the .name.
+//result is returned.
