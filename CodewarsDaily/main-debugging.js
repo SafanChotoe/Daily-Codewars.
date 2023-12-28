3294014ab71704cfdb7312449849d4ddbba390c0
Safan Chotoe <147611100+SafanChotoe@users.noreply.github.com>
2023-12-28 23:16:20 +0100

    Add files via upload

---
+++ CodewarsDaily/main-debugging.js
@@ -0,0 +1,9 @@
+// I would like to be able to pass an array with two elements to my swapValues function to swap the values. However it appears that the values aren't changing.
+
+// Can you figure out what's wrong here?
+function swapValues(arr) {
+    return arr.reverse();
+}
+
+// reverse method in js simply reverses the element order in the given Array.apply
+//Amazingly simple!
