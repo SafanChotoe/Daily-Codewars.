ef177e8a34fb26dace88c7645db1ebd765782e5c
Safan Chotoe <147611100+SafanChotoe@users.noreply.github.com>
2024-01-12 23:59:03 +0100

    Add files via upload

---
+++ CodewarsDaily/main-list.js
@@ -0,0 +1,25 @@
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// The pipes are correct when each pipe after the first is 1 more than the previous one.
+
+// Task
+// Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+// LISTSFUNDAMENTALS
+
+function pipeFix(numbers){
+    let first = numbers[0];
+    let last = numbers[numbers.length-1];
+    
+    let arr = [];
+    for(var i = first; i <= last; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
